338f39a835454808b2e660a0a467ea7418d66cc9
Joshua Durham <jdurham9725@eagle.fgcu.edu>
2023-09-12 18:51:14 -0400

    added links to social icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -50,9 +50,9 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
+                <a href="https://www.instagram.com/josh.thewebdev/"><img src={navIcon1} alt="" /></a>
+                
+                <a href="https://www.linkedin.com/in/joshuadurham38/"><img src={navIcon3} alt="" /></a>
               </div>
               <HashLink to='#connect'>
                 <button className="vvd"><span>Let’s Connect</span></button>
